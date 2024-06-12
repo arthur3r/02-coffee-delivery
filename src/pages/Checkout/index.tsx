974c279ef5f934bff63bb3e6f8a3fd9a493e4f6c
arthur3r <arthurfreireribeiro19@gmail.com>
2024-06-12 19:37:08 -0300

    feat: add section coffees selected

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,5 +1,6 @@
 import { AddressForm } from './components/AddressForm'
 import { FormOfPayment } from './components/FormOfPayment'
+import { CoffeesSelected } from './components/CoffeesSelected'
 import { CheckoutContainer } from './styles'
 
 export function Checkout() {
@@ -9,6 +10,11 @@ export function Checkout() {
         <h2>Complete seu pedido</h2>
         <AddressForm />
         <FormOfPayment />
+      </section>
+
+      <section>
+        <h2>Cafés selecionados</h2>
+        <CoffeesSelected />
       </section>
     </CheckoutContainer>
   )
